9291f1f61c7cba74a8f12420e65effdc934d6bea
Jerrison Chang-Sundin <cchang5@albany.edu>
2019-09-27 15:45:15 -0400

    trying to do the login logout thing but is still messy

--- RateMyServices/urls.py
+++ RateMyServices/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	path('<int:generaluser_id>/', views.search, name='search'),
 	path('<int:professionaluser_id>&<int:generaluser_id>/', views.user_profile, name='user_profile'),
-	path('<int:professionaluser_id>&<int:generaluser_id>/rate/', views.rate, name='rate')
+	path('<int:professionaluser_id>&<int:generaluser_id>/rate/', views.rate, name='rate'),
+	path('login', views.login, name='login'),
+	path('submit_login',views.submit_login, name='submit_login'),
+	path('signup', views.signup, name='signup'),
+	path('submit_logout', views.logout, name='submit_logout')
 ]
--- RateMyServices/views.py
+++ RateMyServices/views.py
@@ -4,6 +4,8 @@ from .models import *
 from django.template import *
 from django.urls import *
 from django.views import *
+from django.contrib.auth import *
+
 
 # Create your views here.
 def user_profile(request, professionaluser_id, generaluser_id):
@@ -17,9 +19,9 @@ def rate(request, professionaluser_id, generaluser_id):
 	selected_rating = pUser.rating_set.create(rater=gUser, provider=pUser, rating=int(request.POST['rating']), description=request.POST['description'])
 	return HttpResponseRedirect(reverse('RateMyServices:index'))
 
-def index(request):
+def index(request): 
 	pUsers = ProfessionalUser.objects.all()
-	gUser = GeneralUser.objects.get(id=2) #for testing purposes
+	gUser = GeneralUser.objects.get(id= 2) #id = 2 : for testing purposes
 	context = {
 		'pUsers': pUsers,
 		'gUser': gUser
@@ -34,3 +36,22 @@ def search(request, generaluser_id):
 		'gUser': gUser
 	}
 	return render(request, 'RateMyServices/results.html', context)
+
+def signup(request):
+	return render(request, 'RateMyServices/signuppage.html')
+
+def login(request):
+	return render(request, 'RateMyServices/loginpage.html')
+
+def submit_login(request):
+	#this is not working yet
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(request, username = username, password = password)
+	if user is not None:
+		return HttpResponseRedirect(reverse('RateMyServices:index'))
+
+def submit_logout(request):
+	#this is not working properly either, will log out but redirect doesn't work
+	logout(request)
+	return render(request,'RateMyServices/index.html')
