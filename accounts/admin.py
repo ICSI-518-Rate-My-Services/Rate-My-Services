e704770cb8772add2466df23ce8c4209b670679d
Jerrison Chang-Sundin <cchang5@albany.edu>
2019-10-12 10:55:31 -0400

    UserAdmin page update

--- accounts/admin.py
+++ accounts/admin.py
@@ -14,7 +14,7 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email','first_name','last_name', 'admin')
+    list_display = ('email','first_name','last_name', 'admin','active','professional')
     list_filter = ('admin','active','staff','professional')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
